efc1adfb90cd5d73d8ed34397f983e308366315a
Arek Jaskólski <arek.jaskolski@gmail.com>
2019-04-20 22:28:31 +0200

    Added validation for CreateVideo

--- apps/learning_object/lib/command_handlers/create_video_handler.ex
+++ apps/learning_object/lib/command_handlers/create_video_handler.ex
@@ -1,10 +1,29 @@
 defmodule LearningObject.CommandHandlers.CreateVideoHandler do
   @behaviour Commanded.Commands.Handler
+  alias Ecto.Changeset
 
   alias LearningObject.Aggregates.Video
   alias LearningObject.Commands.CreateVideo
 
+  @types %{
+    uuid: :string,
+    name: :string,
+    url: :string
+  }
+
   def handle(%Video{uuid: nil} = aggregate, %CreateVideo{} = create_video) do
-    Video.create_video(%Video{}, create_video)
+    changeset = validate(create_video, Map.from_struct(create_video)) 
+    case changeset.valid? do
+      true  -> Video.create_video(%Video{}, create_video)
+      false -> {:error, changeset.errors}
+    end
+  end
+
+  defp  validate(command, params \\ %{}) do
+    fields = Map.keys(@types)
+    changeset =
+      {command, @types}
+      |> Ecto.Changeset.cast(params, fields)
+      |> Ecto.Changeset.validate_required(fields)
   end
 end
--- apps/learning_object/lib/learning_object.ex
+++ apps/learning_object/lib/learning_object.ex
@@ -1,15 +1,17 @@
 defmodule LearningObject do
 
     alias LearningObject.Commands.CreateVideo
-    alias LearningObject.Queries.LearningObject
+    alias LearningObject.Queries.LearningObject, as: Q
     alias LearningObject.Router
 
     @moduledoc """
     Bounded context for Learning Object
     """
-    def create_video(%{name: name, url: url, uuid: uuid} = params) do
+    
+    def create_video(%{uuid: uuid} = params) do
       res = struct(CreateVideo, params)
       |> Router.dispatch(consistency: :strong)
+      
       with :ok <- res do
         {:ok, get(uuid)}
       else
@@ -17,11 +19,11 @@ defmodule LearningObject do
       end
     end
 
-    def create_video(%{name: name, url: url} = params) do
+    def create_video(%{} = params) do
       Map.put(params, :uuid, Ecto.UUID.generate())
       |> create_video()
     end
 
-    defdelegate all(), to: LearningObject
-    defdelegate get(id), to: LearningObject
+    defdelegate all(), to: Q
+    defdelegate get(id), to: Q
 end
